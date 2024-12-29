aaabf51ed866793e33c84527ddcca2d5e0c0f0ba
Alfred Louis <alfredlouisw8@gmail.com>
2024-12-29 11:31:38 +0700

    update

--- src/components/section/PackagesSection.tsx
+++ src/components/section/PackagesSection.tsx
@@ -1,7 +1,14 @@
 import DraggableSlider from '../shared/slider/Slider';
 
 export default function PackagesSection({ packages }) {
-  const multipliedPackages = [...packages, ...packages, ...packages, ...packages];
+  const multipliedPackages = [...Array(10)].map((_, index) => {
+    return {
+      image: packages[0]?.image,
+      title: packages[0]?.title,
+      description: packages[0]?.description,
+      slug: packages[0]?.slug + index,
+    };
+  });
 
   return (
     <div className="flex w-full flex-col justify-between p-10">
