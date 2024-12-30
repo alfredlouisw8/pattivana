13f62af2f3ce9a34f14b413433f21c780db30016
Alfred Louis <alfredlouisw8@gmail.com>
2024-12-30 21:07:58 +0700

    add type

--- src/app/[locale]/(menuLayout)/pdf/page.tsx
+++ src/app/[locale]/(menuLayout)/pdf/page.tsx
@@ -1,5 +1,5 @@
 import PDFSection from '@src/components/section/PDFSection';
 
 export default function PDFPage() {
-  return <PDFSection />;
+  return <PDFSection answers={{}} formData={{}} recommendations={[]} />;
 }
